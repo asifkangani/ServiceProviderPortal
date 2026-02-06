ef394b43d2e9540bf057bd1d433ffe5bb9623316
lomate.ankitha <lomate.ankitha@iamdigitaltrust.com>
2026-02-06 15:34:29 +0530

    update the example to dtt

--- src/main/java/com/example/organization/repository/MetaSoftwareRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.organization.repository;
-
-import com.example.organization.model.MetaSoftwareEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MetaSoftwareRepo extends JpaRepository<MetaSoftwareEntity, Long> {
-}
